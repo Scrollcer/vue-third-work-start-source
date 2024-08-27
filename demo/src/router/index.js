8ae5104dd154a4ef1cc9341e3a139bd80bf81195
LazarevDS <lazarevds@uriit.ru>
2024-08-27 17:12:17 +0500

    module 6 and 7 - finished

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,15 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
-import routes from '../routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: () => import("../components/DemoMenu.vue"),
+      path: "/component-a",
+      name: "ComponentA",
+      component: () => import("../demo6/ComponentA.vue"),
     },
-    ...routes,
+    {
+      path: "/component-b",
+      name: "ComponentB",
+      component: () => import("../demo6/ComponentB.vue"),
+    },
   ],
 });
 
